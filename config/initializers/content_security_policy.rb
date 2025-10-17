73f78c72ab24cbc4161cfa3da84bfc9d407cc016
Nasjwa Eddyani <nasjwaeddyani@hotmail.com>
2025-10-17 14:37:19 +0200

    Allow Cloudinary in CSP (img_src)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,3 +23,19 @@
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
 # end
+
+# config/initializers/content_security_policy.rb
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data, "https://res.cloudinary.com", "https://*.cloudinary.com"
+    policy.object_src  :none
+    policy.script_src  :self, :https
+    policy.style_src   :self, :https, :unsafe_inline
+    policy.connect_src :self, :https
+  end
+
+  # If you want to debug CSP without blocking, uncomment:
+  # config.content_security_policy_report_only = true
+end
